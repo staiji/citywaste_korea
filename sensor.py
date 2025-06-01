62a12d7a6cdc01527530a98396fd72d09874d553
staiji <staiji221@naver.com>
2025-06-01 20:49:53 +0900

    SSLCertVerificationError
    Temporary fix for SSLCertVerificationError

--- sensor.py
+++ sensor.py
@@ -167,7 +167,7 @@ class CityWasteData:
         headers = {'Referer': 'https://www.citywaste.or.kr/portal/status/selectSimpleEmissionQuantity.do'}
 
         try:
-            res = requests.get(REQ_URL, headers = headers)
+            res = requests.get(REQ_URL, headers = headers, verify = False)
             res.raise_for_status()
         except requests.exceptions.HTTPError as errh:
             _LOGGER.error("Http Error: %s", errh)
@@ -208,7 +208,7 @@ class CityWasteData:
                 try:
                     REQ_URL = API_URL + "&pageIndex=" + str(currentPage)
                     _LOGGER.info("REQ_URL %s", REQ_URL)
-                    res = requests.get(REQ_URL, headers = headers)
+                    res = requests.get(REQ_URL, headers = headers, verify = False)
                     res.raise_for_status()
                 except requests.exceptions.HTTPError as errh:
                     _LOGGER.error("Http Error: %s", errh)
